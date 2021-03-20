0aabfae30c1c7d423b8cacbb69762f667c4d2a98
gakitomo <tmo8002@gmail.com>
2021-03-20 23:27:36 +0900

    Update card.blade.php

--- resources/views/articles/card.blade.php
+++ resources/views/articles/card.blade.php
@@ -61,7 +61,7 @@
   </div>
   <div class="card-body pt-0 pb-2 pl-3">
     <div class="card-text">
-      <article-like :initials-is-by='@json($aritcle->isLikeBy(Auth::user()))'>
+      <article-like :initials-is-by='@json($article->isLikeBy(Auth::user()))'>
       </article-like>
     </div>
   </div>
