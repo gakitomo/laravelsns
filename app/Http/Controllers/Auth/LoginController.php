09b252fda46419edf0c5459db37d3792754869f0
gakitomo <tmo8002@gmail.com>
2021-03-27 22:57:43 +0900

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -57,6 +57,10 @@ class LoginController extends Controller
             return $this->sendLoginResponse($request);
         }
         
-        
+        return redirect()->route('register.{provider}', [
+            'provider' => $provider,
+            'email' => $providerUser->getEmail(),
+            'token' => $providerUser->token,
+        ]);    
     }
 }
